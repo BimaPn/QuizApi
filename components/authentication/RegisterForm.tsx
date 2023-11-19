023c975d66dec6d3a433c03a8b5958c6ff5faccf
Bima Pn <bimaptr12@gmail.com>
2023-11-19 17:48:16 +0700

    Connecting client to the backend and save registered user to the database.

--- components/authentication/RegisterForm.tsx
+++ components/authentication/RegisterForm.tsx
@@ -1,28 +1,79 @@
+"use client"
 import Link from "next/link"
 import TextInput from "../ui/TextInput"
 import ButtonPrimary from "../ui/ButtonPrimary"
+import { useState } from "react"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import InputError from "../ui/InputError"
+
+
 
 const RegisterForm = () => {
-  return (
-    <form className="flex flex-col gap-2">
+  const [formData,setFormData] = useState({
+    name : "",
+    email : "",
+    password : "",
+    password_confirmation : "",
+  });
+  const [errors,setErrors] = useState<RegisterErrors|null>(null);
+  const router = useRouter();
 
-      <TextInput 
-      type="text"
-      className="!py-[6px] mb-1"
-      placeholder="Your Name"/>
-      <TextInput
-      type="text"
-      className="!py-[6px] mb-1"
-      placeholder="Your email"/>
-      <TextInput
-      type="password"
-      className="!py-[6px] mb-1"
-      placeholder="Your password"/>
-      <TextInput 
-      type="password"
-      className="!py-[6px]"
-      placeholder="Confirm password"/>
-      <ButtonPrimary className="mt-2">Register</ButtonPrimary>
+    const formSubmit =  (e:React.FormEvent<EventTarget>) => {
+      e.preventDefault()
+
+      axios.post(`http://127.0.0.1:8000/api/auth/register`,formData)
+      .then(() => {
+        router.push("/login");      
+      })
+      .catch(error => {
+        const validatedErrors = error.response.data.errors;
+        setErrors(validatedErrors);
+      });
+    }
+  return (
+    <form onSubmit={formSubmit} className="flex flex-col gap-2">
+      <div>
+        <TextInput 
+        type="text"
+        value={formData.name}
+        onChange={(e) => setFormData({...formData,name:e.target.value})}
+        className="!py-[6px] mb-1"
+        required
+        placeholder="Your Name"/>
+        <InputError message={errors?.name && errors.name[0]} />
+      </div>
+      <div>
+        <TextInput
+        type="email"
+        value={formData.email}
+        onChange={(e) => setFormData({...formData,email:e.target.value})}
+        className="!py-[6px] mb-1"
+        required
+        placeholder="Your email"/>
+        <InputError message={errors?.email && errors.email[0]} />
+      </div>
+      <div>
+        <TextInput 
+        type="password"
+        value={formData.password}
+        onChange={(e) => setFormData({...formData,password:e.target.value})}
+        className="!py-[6px] mb-1"
+        required
+        placeholder="Your Name"/>
+        <InputError message={errors?.password && errors.password[0]} />
+      </div>
+      <div>
+        <TextInput 
+        type="password"
+        value={formData.password_confirmation}
+        onChange={(e) => setFormData({...formData,password_confirmation:e.target.value})}
+        className="!py-[6px] mb-1"
+        required
+        placeholder="Your Name"/>
+      </div>
+      
+      <ButtonPrimary type="submit" className="mt-2">Register</ButtonPrimary>
     </form>
   )
 }
---
+++ types/auth.d.ts
@@ -0,0 +1,5 @@
+interface RegisterErrors {
+  name:string[],
+  email:string[],
+  password:string[]
+}
